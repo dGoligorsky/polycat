6a37ed494a631122f991b9052d034e9d8761032a
David Goligorsky <d.goligorsky@gmail.com>
2018-12-16 13:28:46 -0500

    mapped touch events to the mousemove event for mobile functionality

--- js/cat.js
+++ js/cat.js
@@ -176,6 +176,29 @@ document.addEventListener("wheel", function(event) {
     event.preventDefault()
 })
 
+document.addEventListener("touchstart", touch2Mouse, true);
+document.addEventListener("touchmove", touch2Mouse, true);
+document.addEventListener("touchend", touch2Mouse, true);
+
+function touch2Mouse(e) {
+  var theTouch = e.changedTouches[0];
+  var mouseEv;
+
+  switch(e.type)
+  {
+    case "touchstart": mouseEv="mousedown"; break;  
+    case "touchend":   mouseEv="mouseup"; break;
+    case "touchmove":  mouseEv="mousemove"; break;
+    default: return;
+  }
+
+  var mouseEvent = document.createEvent("MouseEvent");
+  mouseEvent.initMouseEvent(mouseEv, true, true, window, 1, theTouch.screenX, theTouch.screenY, theTouch.clientX, theTouch.clientY, false, false, false, false, 0, null);
+  theTouch.target.dispatchEvent(mouseEvent);
+
+//   e.preventDefault();
+}
+
 window.addEventListener("resize", function() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
